97f2a521da0b0fd02c9761a473919a231c72dbec
Hrishiraj Sawan <hrishiraj102@gmail.com>
2023-01-16 12:59:12 +0530

    React app commit 1

---
+++ app2/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Details from './Details';
+
+function App() {
+  return (
+    <div className="App">
+      <Details />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ app2/src/Details.js
@@ -0,0 +1,7 @@
+export default function Details(){
+    return(
+        <h2>Rohan</h2>
+        
+
+    )
+}
